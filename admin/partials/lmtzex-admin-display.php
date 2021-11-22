616320b9899848dc27d6c3e9da0a6bcff5d1a624
Joanez Andrades <suportedeathofday@gmail.com>
2021-11-22 16:26:46 -0300

    Finalização da página de configurações

--- admin/partials/lmtzex-admin-display.php
+++ admin/partials/lmtzex-admin-display.php
@@ -21,20 +21,29 @@ $weekDays = [
     'Sexta Feira'   => 'friday', 
     'Sábado'        => 'saturday',
 ];
+
+$hours = [
+    '00', '01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23'
+];
+
+$minutes = [
+    '00', '05', '10', '15', '20', '25', '30', '35', '40', '45', '50', '55'
+];
 ?>
 <div id="content">
     <form method="post" action="options.php">
-        <!-- <input type="hidden" name="option_page" value="orddd_date_settings">
-        <input type="hidden" name="action" value="update">
-        <input type="hidden" id="_wpnonce" name="_wpnonce" value="040b82f23f">
-        <input type="hidden" name="_wp_http_referer" value="/woocommercepro/wp-admin/admin.php?page=order_delivery_date"> -->
+        <?php
+        $option_group = 'lmtzez_settings';
+        settings_fields( $option_group );
+        do_settings_sections( $option_group );
+        ?>
         <h2>Lummertz Express - Configurações</h2>
         <table class="form-table" role="presentation">
             <tbody>
                 <tr>
                     <th scope="row">Habilitar períodos de entrega:</th>
                     <td>
-                        <input type="checkbox" name="_lmrtz_period_active" id="_lmrtz_period_active" class="">
+                        <input type="checkbox" name="_lmrtz_period_active" id="_lmrtz_period_active" class="" <?php echo get_option( '_lmrtz_period_active' ) != false ? 'checked="checked"' : ''; ?> >
                         <label for="_lmrtz_period_active">Ative para escolher o período de entrega na página de checkout.</label>
                     </td>
                 </tr>
@@ -43,9 +52,14 @@ $weekDays = [
                     <td>
                         <?php 
                         foreach( $weekDays as $key => $value ){
-                            $valueKey = '_lmrtz_delivery_active_' . strtolower( $value );
+                            $valueKey   = '_lmrtz_delivery_active_' . strtolower( $value );
+                            $checked    = get_option( $valueKey );
+                            $isChecked  = '';
+                            if( $checked ){
+                                $isChecked = 'checked="checked"';
+                            }
                             echo '<div class="">
-                                <input type="checkbox" name="'. $valueKey .'" id="'. $valueKey .'" class="">
+                                <input type="checkbox" name="'. $valueKey .'" id="'. $valueKey .'" class="" '. $isChecked .'>
                                 <label for="'. $valueKey .'">'. $key .'</label>
                             </div>';
                         }
@@ -58,88 +72,72 @@ $weekDays = [
                     <td>
                         <div class="" style="margin-bottom: 4px;">
                             <label for="">Das: </label>
-                            <select name="_lmtzex_delivery_from_hours" id="_lmtzex_delivery_from_hours" size="1">
-                                <option selected="selected" value="1">1</option>
-                                <option value="2">2</option>
-                                <option value="3">3</option>
-                                <option value="4">4</option>
-                                <option value="5">5</option>
-                                <option value="6">6</option>
-                                <option value="7">7</option>
-                                <option value="8">8</option>
-                                <option value="9">9</option>
-                                <option value="10">10</option>
-                                <option value="11">11</option>
-                                <option value="12">12</option>
-                                <option value="13">13</option>
-                                <option value="14">14</option>
-                                <option value="15">15</option>
-                                <option value="16">16</option>
-                                <option value="17">17</option>
-                                <option value="18">18</option>
-                                <option value="19">19</option>
-                                <option value="20">20</option>
-                                <option value="21">21</option>
-                                <option value="22">22</option>
-                                <option value="23">23</option>
+                            <select name="_lmtzex_delivery_start_hour" id="_lmtzex_delivery_start_hour" size="1">
+                                <?php
+                                $selectedHour   = get_option( '_lmtzex_delivery_start_hour' );
+                                $options        = '';
+                                foreach( $hours as $hour ){
+                                    if( $selectedHour == $hour ){
+                                        $options .= '<option selected="selected" value="'. $hour .'">'. $hour .'</option>';
+                                    } else {
+                                        $options .= '<option value="'. $hour .'">'. $hour .'</option>';
+                                    }
+                                }
+
+                                echo $options;
+                                ?>
                             </select>
-                            <select name="_lmtzex_delivery_from_mins" id="_lmtzex_delivery_from_mins" size="1">
-                                <option selected="selected" value="0">0</option>
-                                <option value="5">5</option>
-                                <option value="10">10</option>
-                                <option value="15">15</option>
-                                <option value="20">20</option>
-                                <option value="25">25</option>
-                                <option value="30">30</option>
-                                <option value="35">35</option>
-                                <option value="40">40</option>
-                                <option value="45">45</option>
-                                <option value="50">50</option>
-                                <option value="55">55</option>
-                            </select>                            
+                            <select name="_lmtzex_delivery_start_min" id="_lmtzex_delivery_start_min" size="1">
+                                <?php 
+                                $selectedMinute = get_option( '_lmtzex_delivery_start_min' );
+                                unset( $options );
+                                $options        = '';
+                                foreach( $minutes as $minute ){
+                                    if( $selectedMinute == $minute ){
+                                        $options .= '<option selected="selected" value="'. $minute .'">'. $minute .'</option>';
+                                    } else {
+                                        $options .= '<option value="'. $minute .'">'. $minute .'</option>';
+                                    }
+                                }
+
+                                echo $options;
+                                ?>
+                            </select>               
                         </div>
                         <div class="">
                             <label for="">Até: </label>
-                            <select name="_lmtzex_delivery_from_hours" id="_lmtzex_delivery_from_hours" size="1">
-                                <option selected="selected" value="1">1</option>
-                                <option value="2">2</option>
-                                <option value="3">3</option>
-                                <option value="4">4</option>
-                                <option value="5">5</option>
-                                <option value="6">6</option>
-                                <option value="7">7</option>
-                                <option value="8">8</option>
-                                <option value="9">9</option>
-                                <option value="10">10</option>
-                                <option value="11">11</option>
-                                <option value="12">12</option>
-                                <option value="13">13</option>
-                                <option value="14">14</option>
-                                <option value="15">15</option>
-                                <option value="16">16</option>
-                                <option value="17">17</option>
-                                <option value="18">18</option>
-                                <option value="19">19</option>
-                                <option value="20">20</option>
-                                <option value="21">21</option>
-                                <option value="22">22</option>
-                                <option value="23">23</option>
+                            <select name="_lmtzex_delivery_end_hour" id="_lmtzex_delivery_end_hour" size="1">
+                                <?php
+                                $selectedHour   = get_option( '_lmtzex_delivery_end_hour' );
+                                unset( $options );
+                                $options        = '';
+                                foreach( $hours as $hour ){
+                                    if( $selectedHour == $hour ){
+                                        $options .= '<option selected="selected" value="'. $hour .'">'. $hour .'</option>';
+                                    } else {
+                                        $options .= '<option value="'. $hour .'">'. $hour .'</option>';
+                                    }
+                                }
+
+                                echo $options;
+                                ?>
                             </select>
-                            <select name="_lmtzex_delivery_from_mins" id="_lmtzex_delivery_from_mins" size="1">
-                                <option selected="selected" value="0">0</option>
-                                <option value="5">5</option>
-                                <option value="10">10</option>
-                                <option value="15">15</option>
-                                <option value="20">20</option>
-                                <option value="25">25</option>
-                                <option value="30">30</option>
-                                <option value="35">35</option>
-                                <option value="40">40</option>
-                                <option value="45">45</option>
-                                <option value="50">50</option>
-                                <option value="55">55</option>
+                            <select name="_lmtzex_delivery_end_min" id="_lmtzex_delivery_end_min" size="1">
+                                <?php 
+                                $selectedMinute = get_option( '_lmtzex_delivery_end_min' );
+                                unset( $options );
+                                $options        = '';
+                                foreach( $minutes as $minute ){
+                                    if( $selectedMinute == $minute ){
+                                        $options .= '<option selected="selected" value="'. $minute .'">'. $minute .'</option>';
+                                    } else {
+                                        $options .= '<option value="'. $minute .'">'. $minute .'</option>';
+                                    }
+                                }
+
+                                echo $options;
+                                ?>
                             </select>
-                            
                         </div>
                         <em for="">Selecione o período de entregas durante o dia.</em>
                     </td>
@@ -147,8 +145,8 @@ $weekDays = [
                 <tr>
                     <th scope="row">Habilitar entrega no mesmo dia:</th>
                     <td>
-                        <input type="checkbox" name="_lmrtz_same_day_delivery" id="_lmrtz_same_day_delivery" class="">
-                        <label for="_lmrtz_same_day_delivery">Habilite a entrega no mesmo dia para os pedidos.</label>
+                        <input type="checkbox" name="_lmrtz_same_day_delivery" id="_lmrtz_same_day_delivery" class="" <?php echo get_option( '_lmrtz_same_day_delivery' ) != false ? 'checked="checked"' : ''; ?>>
+                        <label for="_lmrtz_same_day_delivery">Habilite a entrega no mesmo dia.</label>
                     </td>
                 </tr>
                 <tr>
@@ -156,46 +154,38 @@ $weekDays = [
                     <td>
                         <div class="">
                             <select name="_lmrtz_same_day_delivery_hour_limit" id="_lmrtz_same_day_delivery_hour_limit" size="1">
-                                <option selected="selected" value="1">1</option>
-                                <option value="2">2</option>
-                                <option value="3">3</option>
-                                <option value="4">4</option>
-                                <option value="5">5</option>
-                                <option value="6">6</option>
-                                <option value="7">7</option>
-                                <option value="8">8</option>
-                                <option value="9">9</option>
-                                <option value="10">10</option>
-                                <option value="11">11</option>
-                                <option value="12">12</option>
-                                <option value="13">13</option>
-                                <option value="14">14</option>
-                                <option value="15">15</option>
-                                <option value="16">16</option>
-                                <option value="17">17</option>
-                                <option value="18">18</option>
-                                <option value="19">19</option>
-                                <option value="20">20</option>
-                                <option value="21">21</option>
-                                <option value="22">22</option>
-                                <option value="23">23</option>
+                            <?php
+                                $selectedHour   = get_option( '_lmrtz_same_day_delivery_hour_limit' );
+                                unset( $options );
+                                $options        = '';
+                                foreach( $hours as $hour ){
+                                    if( $selectedHour == $hour ){
+                                        $options .= '<option selected="selected" value="'. $hour .'">'. $hour .'</option>';
+                                    } else {
+                                        $options .= '<option value="'. $hour .'">'. $hour .'</option>';
+                                    }
+                                }
+
+                                echo $options;
+                                ?>
                             </select>
-                            <select name="_lmrtz_same_day_delivery_hour_limit" id="_lmrtz_same_day_delivery_hour_limit" size="1">
-                                <option selected="selected" value="0">0</option>
-                                <option value="5">5</option>
-                                <option value="10">10</option>
-                                <option value="15">15</option>
-                                <option value="20">20</option>
-                                <option value="25">25</option>
-                                <option value="30">30</option>
-                                <option value="35">35</option>
-                                <option value="40">40</option>
-                                <option value="45">45</option>
-                                <option value="50">50</option>
-                                <option value="55">55</option>
+                            <select name="_lmrtz_same_day_delivery_min_limit" id="_lmrtz_same_day_delivery_min_limit" size="1">
+                                <?php 
+                                $selectedMinute = get_option( '_lmrtz_same_day_delivery_min_limit' );
+                                unset( $options );
+                                $options        = '';
+                                foreach( $minutes as $minute ){
+                                    if( $selectedMinute == $minute ){
+                                        $options .= '<option selected="selected" value="'. $minute .'">'. $minute .'</option>';
+                                    } else {
+                                        $options .= '<option value="'. $minute .'">'. $minute .'</option>';
+                                    }
+                                }
+
+                                echo $options;
+                                ?>
                             </select>
                         </div>
-                        <!-- <input type="checkbox" name="_lmrtz_same_day_delivery_hour_limit" id="_lmrtz_same_day_delivery_hour_limit" class=""> -->
                         <label for="_lmrtz_same_day_delivery_hour_limit">Entrega para dia atual será desativado se um pedido for feito após a hora mencionada neste campo.</label>
                     </td>
                 </tr>
@@ -203,24 +193,22 @@ $weekDays = [
                     <th scope="row">Cores no calendário:</th>
                     <td>
                         <div class="">
-                            <input type="color" name="" id="" value="#dd9933">
-                            <label for="">Feriados</label>
+                            <input type="color" name="_lmtzex_color_holidays" id="_lmtzex_color_holidays" value="<?php echo get_option( '_lmtzex_color_holidays' ) != false ? get_option( '_lmtzex_color_holidays' ) : '#dd9933'; ?>">
+                            <label for="_lmtzex_color_holidays">Feriados</label>
                         </div>
                         <div class="">
-                            <input type="color" name="" id="" value="#dd7171">
-                            <label for="">Horário limite sobre datas</label>
+                            <input type="color" name="_lmtzex_color_hour_limit" id="_lmtzex_color_hour_limit" value="<?php echo get_option( '_lmtzex_color_hour_limit' ) != false ? get_option( '_lmtzex_color_hour_limit' ) : '#dd7171'; ?>">
+                            <label for="_lmtzex_color_hour_limit">Horário limite sobre datas</label>
                         </div>
                         <div class="">
-                            <input type="color" name="" id=""value="#a0d67a">
-                            <label for="">Datas disponíveis</label>
+                            <input type="color" name="_lmtzex_color_available" id="_lmtzex_color_available"value="<?php echo get_option( '_lmtzex_color_available' ) != false ? get_option( '_lmtzex_color_available' ) : '#a0d67a'; ?>">
+                            <label for="_lmtzex_color_available">Datas disponíveis</label>
                         </div>
                         <label for="">Cores no calendário para representar dias e horários disponíveis para entregas.</label>
                     </td>
                 </tr>
             </tbody>
         </table>
-        <p class="submit">
-            <input type="submit" name="save" id="save" class="button button-primary" value="Save Settings">
-        </p>
+        <?php submit_button( ); ?>
     </form>           
 </div>
