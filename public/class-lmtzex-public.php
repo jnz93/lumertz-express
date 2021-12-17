3be5510371527160d4e64554a796c6e9b5550796
Joanez Andrades <suportedeathofday@gmail.com>
2021-12-17 12:16:33 -0300

    Várias alterações e ajustes em métodos da classe Lmtzex_Public

--- public/class-lmtzex-public.php
+++ public/class-lmtzex-public.php
@@ -52,6 +52,10 @@ class Lmtzex_Public {
 		$this->plugin_name = $plugin_name;
 		$this->version = $version;
 
+		add_action( 'woocommerce_cart_totals_before_shipping', array( $this, 'displayCustomCheckoutField' ), 10, 1 ); # Mostrar input no carrinho
+		// add_action( 'woocommerce_review_order_after_cart_contents', array( $this, 'displayCustomCheckoutField' ), 10, 1 ); # Mostrar input no checkout
+		// Sempre que o escopo a box de review recarrega um novo campo é adicionado.
+		
 		add_action('woocommerce_checkout_update_order_meta', array( $this, 'saveOrderDeliveryDate'), 10, 1 );
 	}
 
@@ -101,12 +105,18 @@ class Lmtzex_Public {
 		 * class.
 		 */
 
-		wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/lmtzex-public.js', array( 'jquery' ), $this->version, false );
-
 		if( is_checkout() || is_cart() ){
+			wp_enqueue_script( 'lmtzex-datetime', plugin_dir_url( __FILE__ ) . 'js/lmtzex-public.js', array( 'jquery' ), $this->version, false );
 			wp_enqueue_script( 'datetimepicker', plugin_dir_url( __FILE__ ) . 'js/jquery.datetimepicker.full.min.js', array( 'jquery' ), $this->version, false );
-		}
 
+			wp_localize_script( 'lmtzex-datetime', 'delivery', array(
+				'nonce'     		=> wp_create_nonce( 'charts-nonce' ),
+				'hours'  			=> $this->deliveryHours(),
+				'disabledWeekdays'  => $this->disabledWeekDays(),
+				'disabledDatesList' => $this->disabledDates(),
+				'isEnabledSameday'  => $this->deliverySameDayActived(),
+			) );
+		}
 	}
 
 	/**
@@ -150,156 +160,18 @@ class Lmtzex_Public {
 			# Actived settings
 			$isActivated 		= get_option( '_lmrtz_period_active', true );
 	
-			# Definição dos dias ativados e desativados
-			$enabledWeekDays 	= array();
-			$disabledWeekDays 	= array();
-			$weekDays 		 	= array(
-				'sunday'	=> '0',
-				'monday'	=> '1',
-				'tuesday'	=> '2',
-				'wednesday'	=> '3',
-				'thursday'	=> '4',
-				'friday'	=> '5',
-				'saturday'	=> '6',
-			);
-			$keysOfWeek 		= array(
-				'sunday'	=> '_lmrtz_delivery_active_sunday',
-				'monday'	=> '_lmrtz_delivery_active_monday',
-				'tuesday'	=> '_lmrtz_delivery_active_tuesday',
-				'wednesday'	=> '_lmrtz_delivery_active_wednesday',
-				'thursday'	=> '_lmrtz_delivery_active_thursday',
-				'friday'	=> '_lmrtz_delivery_active_friday',
-				'saturday'	=> '_lmrtz_delivery_active_saturday',
-			);
-			foreach( $keysOfWeek as $day => $key ){
-				$data = get_option( $key, true );
-				if( $data ){
-					$enabledWeekDays[] = $day;
-				} else {
-					$disabledWeekDays[] = (int) $weekDays[$day];
-				}
-			}
-			$disabledWeekDays = json_encode( $disabledWeekDays );
-
-			# Definição de horários ativados
-			$deliveryStartHour 	= get_option( '_lmtzex_delivery_start_hour', true );
-			$deliveryStartMin 	= get_option( '_lmtzex_delivery_start_min', true );
-			$deliveryStart 		= $deliveryStartHour . ':' . $deliveryStartMin;
-
-			$deliveryEndHour 	= get_option( '_lmtzex_delivery_end_hour', true );
-			$deliveryEndMin 	= get_option( '_lmtzex_delivery_end_min', true );
-			$deliveryEnd 		= $deliveryEndHour . ':' . $deliveryEndMin;
-
-			$hours 				= array(
-				'00:00',
-				'00:30',
-				'01:00',
-				'01:30',
-				'02:00',
-				'02:30',
-				'03:00',
-				'03:30',
-				'04:00',
-				'04:30',
-				'05:00',
-				'05:30',
-				'06:00',
-				'06:30',
-				'07:00',
-				'07:30',
-				'08:00',
-				'08:30',
-				'09:00',
-				'09:30',
-				'10:00',
-				'10:30',
-				'11:00',
-				'11:30',
-				'12:00',
-				'12:30',
-				'13:00',
-				'13:30',
-				'14:00',
-				'14:30',
-				'15:00',
-				'15:30',
-				'16:00',
-				'16:30',
-				'17:00',
-				'17:30',
-				'18:00',
-				'18:30',
-				'19:00',
-				'19:30',
-				'20:00',
-				'20:30',
-				'21:00',
-				'21:30',
-				'22:00',
-				'22:30',
-				'23:00',
-				'23:30',
-			);
-			$posStartHour 		= array_search( $deliveryStart, $hours );
-			$posEndHour 		= array_search( $deliveryEnd, $hours);
-			$deliveryHours 		= json_encode( array_slice( $hours, $posStartHour, ( $posEndHour+1 - $posStartHour ) ) );
-
 			# Habilitado entrega no mesmo dia
-			$isSameDayActivated 	= get_option( '_lmrtz_same_day_delivery', true );
-			$sameDayHourLimit 		= get_option( '_lmrtz_same_day_delivery_hour_limit', true );
-			$sameDayMinLimit 		= get_option( '_lmrtz_same_day_delivery_min_limit', true );
-			$sameDayLimit 			= $sameDayHourLimit . ':' . $sameDayMinLimit;
-			
-			# Definição de datas desabilitadas
-			$disabledDates 			= array(); # Dias da semana
-			$currentHour			= date_i18n( 'H:i' );
-			$currentDate 			= date_i18n( 'd/m/Y' );
-			$isEnabledSameDay 		= 1; # 0=não/1=sim
-			if( $currentHour > $sameDayLimit ){
-				$isEnabledSameDay 	= 0;
-				$disabledDates[]	= $currentDate;
-			}
-			$daysOff 				= get_option( '_lmtzex_daysoff', true );
-			$daysOffArr 			= array_filter( explode(',', $daysOff ) );
-			foreach( $daysOffArr as $dayOff )
-			{
-				$disabledDates[] = trim($dayOff);
-			}
-			$disabledDates 		= json_encode( $disabledDates );
+			$isSameDayActivated 	= get_option( '_lmrtz_same_day_delivery', true );			
 
 			$holidaysColor 		= get_option( '_lmtzex_color_holidays', true );
 			$hourLimitColor		= get_option( '_lmtzex_color_hour_limit', true );
 			$availableColor 	= get_option( '_lmtzex_color_available', true );
 
 			?>
-			<script>
-			jQuery.datetimepicker.setLocale('pt-BR');
-			jQuery(function($){
-				var d = '#datetimepicker',
-					f = d+'_field',
-					hours = <?php echo $deliveryHours; ?>,
-					disabledWeekdays = <?php echo $disabledWeekDays; ?>,
-					disabledDatesList = <?php echo $disabledDates; ?>;
-					
-				$(d).datetimepicker({
-					format: 'd.m.Y H:i',
-					allowTimes: hours,
-					disabledWeekDays: disabledWeekdays,
-					disabledDates: disabledDatesList,
-					formatDate: 'd/m/Y',
-					minDate: '-1970/01/1',
-					setLocale: 'pt-BR'
-				});
-
-				$(d).change( function(){
-					if( jQuery(this).val() != '' ){
-						jQuery('.shipping__table, .shipping__table--multiple').show();
-					} else {
-						jQuery('.shipping__table, .shipping__table--multiple').hide();
-					}
-				});
-			});
-			</script>
+			<style>
+				/* Desabilitar lista de opções de entrega */
+				.shipping__table{ display: none; }
+			</style>
 			<?php
 	
 		endif;
@@ -313,6 +185,170 @@ class Lmtzex_Public {
 	public function saveOrderDeliveryDate( $order_id ) {
 		if( !empty( $_POST['lmtzex_delivery_date'] ) )
 			update_post_meta( $order_id, 'lmtzex_delivery_date', sanitize_text_field( $_POST['lmtzex_delivery_date'] ) );
+	}
+
+
+	/** Método auxiliares para pegar dados de configurações */
+
+	/**
+	 * Método responsável por retornar um array de horas
+	 * 
+	 */
+	public function deliveryHours()
+	{
+		# Definição de horários ativados
+		$deliveryStartHour 	= get_option( '_lmtzex_delivery_start_hour', true );
+		$deliveryStartMin 	= get_option( '_lmtzex_delivery_start_min', true );
+		$deliveryStart 		= $deliveryStartHour . ':' . $deliveryStartMin;
+
+		$deliveryEndHour 	= get_option( '_lmtzex_delivery_end_hour', true );
+		$deliveryEndMin 	= get_option( '_lmtzex_delivery_end_min', true );
+		$deliveryEnd 		= $deliveryEndHour . ':' . $deliveryEndMin;
+
+		$hours 				= array(
+			'00:00',
+			'00:30',
+			'01:00',
+			'01:30',
+			'02:00',
+			'02:30',
+			'03:00',
+			'03:30',
+			'04:00',
+			'04:30',
+			'05:00',
+			'05:30',
+			'06:00',
+			'06:30',
+			'07:00',
+			'07:30',
+			'08:00',
+			'08:30',
+			'09:00',
+			'09:30',
+			'10:00',
+			'10:30',
+			'11:00',
+			'11:30',
+			'12:00',
+			'12:30',
+			'13:00',
+			'13:30',
+			'14:00',
+			'14:30',
+			'15:00',
+			'15:30',
+			'16:00',
+			'16:30',
+			'17:00',
+			'17:30',
+			'18:00',
+			'18:30',
+			'19:00',
+			'19:30',
+			'20:00',
+			'20:30',
+			'21:00',
+			'21:30',
+			'22:00',
+			'22:30',
+			'23:00',
+			'23:30',
+		);
+		$posStartHour 		= array_search( $deliveryStart, $hours );
+		$posEndHour 		= array_search( $deliveryEnd, $hours);
+		$deliveryHours 		= json_encode( array_slice( $hours, $posStartHour, ( $posEndHour+1 - $posStartHour ) ) );
+
+		return $deliveryHours;
+	}
+
+	/**
+	 * Método responsável por retornar datas desabilitadas
+	 * 
+	 */
+	public function disabledDates()
+	{
+		# Definição de datas desabilitadas
+		$sameDayHourLimit 		= get_option( '_lmrtz_same_day_delivery_hour_limit', true );
+		$sameDayMinLimit 		= get_option( '_lmrtz_same_day_delivery_min_limit', true );
+		$sameDayLimit 			= $sameDayHourLimit . ':' . $sameDayMinLimit;
+		$disabledDates 			= array(); # Dias da semana
+		$currentHour			= date_i18n( 'H:i' );
+		$currentDate 			= date_i18n( 'd/m/Y' );
+		$isEnabledSameDay 		= 1; # 0=não/1=sim
+		if( $currentHour > $sameDayLimit ){
+			$isEnabledSameDay 	= 0;
+			$disabledDates[]	= $currentDate;
+		}
+		$daysOff 				= get_option( '_lmtzex_daysoff', true );
+		$daysOffArr 			= array_filter( explode(',', $daysOff ) );
+		foreach( $daysOffArr as $dayOff )
+		{
+			$disabledDates[] = trim($dayOff);
+		}
+		$disabledDates 		= json_encode( $disabledDates );
+	
+		return $disabledDates;
+	}
+
+	/**
+	 * Método responsável por retornar dias da semana desabilitados
+	 * 
+	 */
+	public function disabledWeekDays()
+	{
+		# Definição dos dias ativados e desativados
+		$enabledWeekDays 	= array();
+		$disabledWeekDays 	= array();
+		$weekDays 		 	= array(
+			'sunday'	=> '0',
+			'monday'	=> '1',
+			'tuesday'	=> '2',
+			'wednesday'	=> '3',
+			'thursday'	=> '4',
+			'friday'	=> '5',
+			'saturday'	=> '6',
+		);
+		$keysOfWeek 		= array(
+			'sunday'	=> '_lmrtz_delivery_active_sunday',
+			'monday'	=> '_lmrtz_delivery_active_monday',
+			'tuesday'	=> '_lmrtz_delivery_active_tuesday',
+			'wednesday'	=> '_lmrtz_delivery_active_wednesday',
+			'thursday'	=> '_lmrtz_delivery_active_thursday',
+			'friday'	=> '_lmrtz_delivery_active_friday',
+			'saturday'	=> '_lmrtz_delivery_active_saturday',
+		);
+		foreach( $keysOfWeek as $day => $key ){
+			$data = get_option( $key, true );
+			if( $data ){
+				$enabledWeekDays[] = $day;
+			} else {
+				$disabledWeekDays[] = (int) $weekDays[$day];
+			}
+		}
+		$disabledWeekDays = json_encode( $disabledWeekDays );
+
+		return $disabledWeekDays;
+	}
+
+	/**
+	 * Método responsável por retornar bool para entrega no mesmo dia
+	 * 
+	 */
+	public function deliverySameDayActived()
+	{
+		# Definição de datas desabilitadas
+		$sameDayHourLimit 		= get_option( '_lmrtz_same_day_delivery_hour_limit', true );
+		$sameDayMinLimit 		= get_option( '_lmrtz_same_day_delivery_min_limit', true );
+		$sameDayLimit 			= $sameDayHourLimit . ':' . $sameDayMinLimit;
+		$disabledDates 			= array(); # Dias da semana
+		$currentHour			= date_i18n( 'H:i' );
+		$currentDate 			= date_i18n( 'd/m/Y' );
+		$isEnabledSameDay 		= 1; # 0=não/1=sim
+		if( $currentHour > $sameDayLimit ){
+			$isEnabledSameDay 	= 0;
+		}
+		return $isEnabledSameDay;
 	}
 
 }
